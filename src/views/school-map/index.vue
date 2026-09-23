1a24e45700b1115e9ff59fca17c38a30bc8fbd2b
zhangxiaohan <3245872096@qq.com>
2026-09-23 11:31:30 +0800

    feat: 添加生产环境配置文件，更新 API 基础 URL；修改页面标题以更准确反映内容

--- src/views/school-map/index.vue
+++ src/views/school-map/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="map-page">
     <div class="header">
-      <h2>太原理工大学明向校区 - 流浪宠物救助地图</h2>
+      <h2>太原理工大学明向校区 - 流浪动物救助地图</h2>
     </div>
     
     <div id="map" class="map-container"></div>
